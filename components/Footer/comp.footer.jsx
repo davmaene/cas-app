15eab32360ca37e0d5d36c0f90738fdc5944e9dc
davmaene <davidmened@gmail.com>
2023-08-12 13:52:49 +0200

    up me line

--- components/Footer/comp.footer.jsx
+++ components/Footer/comp.footer.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View, Text, Keyboard } from "react-native";
 import { Colors } from '../../assets/colors/Colors';
-import { appname } from "../../assets/configs/configs";
+import { appname, longappname } from "../../assets/configs/configs";
 
 export const Footer = () => {
     const [shown, setshown] = React.useState(true);
@@ -22,7 +22,7 @@ export const Footer = () => {
         shown 
         ? 
             <View style={{position: "absolute", bottom: "0%", width: "100%", backgroundColor: Colors.whiteColor, paddingVertical: 10, alignItems: "center", alignSelf: "center"}}>
-                <Text style={{ fontFamily: "mons-b", fontSize: 10, textAlign: "center" }}>&copy; {appname} | {appname} {new Date().getFullYear()} </Text>
+                <Text style={{ fontFamily: "mons-b", fontSize: 10, textAlign: "center" }}>&copy; {appname} | {longappname} {new Date().getFullYear()} </Text>
             </View> 
         :
             <></> 
